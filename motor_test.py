c2dc03ad992caa7552c75cdd4a54f2f44db586a7
Grace Gao <gegao@wisc.edu>
2025-02-14 16:28:53 -0600

    make motor run

--- motor_test.py
+++ motor_test.py
@@ -1,14 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 
+PWM_PIN = 12
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(12, GPIO.OUT)
+GPIO.setup(PWM_PIN, GPIO.OUT)
 
 # Create a PWM instance with a frequency of 50Hz (20ms period)
-pwm = GPIO.PWM(12, 50)
+pwm = GPIO.PWM(PWM_PIN, 50)
+pwm.start(5)
 
-# PWM signal with 25% duty cycle
-def motor_on():
-    pwm.start(25)  # Start PWM with 25% duty cycle
-    time.sleep(1.5)  # Keep the motor on for 1.5 seconds
-    pwm.stop()  # Stop PWM
+try:
+    while True:
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pwm.stop()
+    GPIO.cleanup()
+    print("stopped")
