f6a31c8fb31381740abeccea3613c79db31c221d
Grace Gao <gegao@wisc.edu>
2025-02-21 17:02:38 -0600

    create boat ui, looks gross but works

---
+++ boat-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MapComponent from "./MapComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Boat Map</h1>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
+
