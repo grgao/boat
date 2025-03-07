bc45fb46bdb06d3ba4b97087c07c33c5e17db88f
glmcgowan <glmcgowan@wisc.edu>
2025-03-07 13:39:03 -0600

    updated transmission loop to add weight

--- power_use.py
+++ power_use.py
@@ -7,7 +7,7 @@
 # cause the boat to move and it'd require more power to get back to the original location and heading. 
 
 import numpy as np
-
+from transmission_loop import average_loop_power
 # List to store parts
 parts = [
     # TODO: Need to properly estimate the power usage and percentage on for each part
@@ -15,6 +15,7 @@ parts = [
     # The power_use is the peak power usage without accounting for time on
 
     {'name': 'Propulsion motor', 'percentage_on': 15, 'power_use': 60},
+    {'name': 'Transmission loop', 'percentage_on': 100, 'power_use': average_loop_power},
     {'name': 'Raspberry Pi', 'percentage_on': 100, 'power_use': 5},
     {'name': 'Servo', 'percentage_on': 1, 'power_use': 10},   # Update Servo power usage and estimation of percentage on
     {'name': 'Sensors', 'percentage_on': 100, 'power_use': 5},
@@ -30,7 +31,7 @@ operating_hours = 24 # Hours, how long the boat will be operating for. See if we
 using_solar = True
 
 solar_panel_wattage = 100 # Watts
-one_cell_voltage = 3.7 # Volts
+CELL_VOLTAGE = 3.7 # Volts
 
 #Battery Specs
 twoS_battery_dict = {'S rating': 2, 'Capacity': 38.38, 'C rating': 50}
@@ -39,14 +40,13 @@ twoS_battery_dict = {'S rating': 2, 'Capacity': 38.38, 'C rating': 50}
 dict_list = [twoS_battery_dict]
 for dictionary in dict_list:
     print(f"Calculating battery specs for {dictionary['S rating']}S battery")
-    battery_voltage = one_cell_voltage * dictionary['S rating'] # 2S battery
+    battery_voltage = CELL_VOLTAGE * dictionary['S rating'] # 2S battery
     battery_capacity_Wh = dictionary['Capacity'] # Wh
     battery_capacity_Ah = battery_capacity_Wh / battery_voltage # Ah
     battery_max_discharge_rating = dictionary['C rating'] # Unitless
     battery_max_discharge_A = battery_max_discharge_rating * battery_capacity_Ah # A
 
-
-    print(f"    Battery capacity: {battery_capacity_Ah:.2f}Ah")
+    print(f"    Battery capacity: {battery_capacity_Ah:.2f}Ah, {battery_capacity_Wh:.2f}Wh")
     print(f"    Maximum discharge: {battery_max_discharge_A:.2f}A")
 
 # Calculate the total power usage
@@ -55,17 +55,17 @@ peak_power_usage = 0
 
 print("Power usage by part:")
 for part in parts:
-    print(f"    Part: {part['name']}, Percentage On: {part['percentage_on']}%, Power Use: {part['power_use']}W")
+    print(f"    Part: {part['name']}, Percentage On: {part['percentage_on']}%, Power Use: {part['power_use']:.2f}W")
     total_power_usage += part['power_use'] * part['percentage_on'] / 100
     peak_power_usage += part['power_use']
 
 daily_energy_usage = total_power_usage * operating_hours # Wh
 energy_generated_per_day = solar_panel_wattage * PV_power_potential if using_solar else 0
 
-print(f"Average power: {total_power_usage}W")
-print(f"Peak power: {peak_power_usage}W")
+print(f"Average power: {total_power_usage:.2f}W")
+print(f"Peak power: {peak_power_usage:.2f}W")
 print(f"Energy used per day: {daily_energy_usage:.2f}Wh")
-print(f"Energy generated per day: {energy_generated_per_day}Wh")
+print(f"Energy generated per day: {energy_generated_per_day:.2f}Wh")
 
 # Need this to be true for fully autonomous system
 print(f"System is autonomous: {energy_generated_per_day > daily_energy_usage}")
--- transmission_loop.py
+++ transmission_loop.py
@@ -2,24 +2,31 @@ import numpy as np
 
 # Goal: design a transmitting loop, with a mag. moment = 50-100 Am^2
 
-N = 50
-I = 1  # Peak AC current
-A = 0.91 * 0.61  # 2x3 ft loop
+# Constants
+METER_TO_FEET = 3.28084
+FEET_TO_METER = 1 / METER_TO_FEET
+LB_TO_KG = 0.453592
+RL_18AWG = 20.9 / 1000  # Ohms per km of wire for #18 AWG
+WEIGHT_18AWG = 0.010 # lbs/ft
+
+N = 75
+I = 1.5  # Peak AC current
+
+width = 2 * FEET_TO_METER # 2 ft loop
+height = 2.5 * FEET_TO_METER # 2.5 ft loop
+
+A = width * height  # 2x3 ft loop
 f = 30  # frequency that you are driving the loop at. This small signal comes from the Raspberry Pi.
 w = 2 * np.pi * f
 
 magnetic_moment = N * I * A  # N is the number of turns, I is the current in the wire, and A is the area of the loop.
 
-METER_TO_FEET = 3.28084
+perimiter = height * 2 + width * 2
 
-
-rl_18AWG = 20.9 / 1000  # Ohms per meter of wire for #18 AWG
-
-
-perimiter = 0.91 * 2 + 0.61 * 2
-R = (perimiter) * N * rl_18AWG
-
+R = perimiter * N * RL_18AWG
 perimiter_feet = perimiter * METER_TO_FEET
+weight_wire = perimiter_feet * WEIGHT_18AWG * N
+
 
 # Inductance?
 # https://www.allaboutcircuits.com/tools/rectangle-loop-inductance-calculator/
@@ -36,7 +43,31 @@ Vpa = 2 * Vp  # DC power amplifier voltage (Max amount we can handle; max amount
 
 Cs = 1 / (L * w**2)  # Series capacitance needed to cancel inductance
 
-P = 0.5 * I**2 * R
+average_loop_power = 0.5 * I**2 * R
+
+
+curr_power = np.inf
+# Iterate through different combinations of N and I to get the desired magnetic moment and minimize power
+
+"""for n in range(1, 100):
+    # Iterate through different currents by 0.5
+    for i_mult in range(0, 100, 5):
+        i = i_mult / 10
+        magnetic_moment = n * i * A
+        if magnetic_moment <= 50:
+            continue
+
+        R = perimiter * n * rl_18AWG
+        P = 0.5 * i**2 * R
+        if P < curr_power:
+            curr_power = P
+            best_N = n
+            best_I = i
+        
+
+print(f"Best N: {best_N}, Best I: {best_I}, Best Power: {curr_power}, magnetic moment: {best_N * best_I * A}")"""
+
+
 
 # Print results
 print(f"Magnetic moment (m): {magnetic_moment:.2f} Am^2")
@@ -47,5 +78,6 @@ print(f"Peak voltage without capacitor (Vp): {Vp:.2f} V")
 print(f"Peak voltage with capacitor (Vp_c): {Vp_c:.2f} V")
 print(f"DC power amplifier voltage (Vpa): {Vpa:.2f} V")
 print(f"Series capacitance (Cs): {Cs:.6f} F")
-print(f"Power (P): {P:.2f} W")
+print(f"Power (P): {average_loop_power:.2f} W")
 print(f"Total Wire needed (ft): {perimiter_feet*N:.2f} ft") 
+print(f"Total Weight of Wire (kg): {weight_wire:.2f} lb")
