50279c3f45f0d9820861476a6850d0cc1df24c12
Grace Gao <gegao@wisc.edu>
2025-03-17 17:19:59 -0500

    ASDF

--- ADC_sine_test.py
+++ ADC_sine_test.py
@@ -9,10 +9,29 @@ i2c = busio.I2C(board.SCL, board.SDA)
 dac = adafruit_mcp4725.MCP4725(i2c, address=0x60)
 
 
-# Test output voltages
-dac.raw_value = 0       # Should output ~0V
-input("Press Enter to continue...")
-dac.raw_value = 2048    # Should output ~1.65V (if VREF = 3.3V)
-input("Press Enter to continue...")
-dac.raw_value = 4095    # Should output ~3.3V (if VREF = 3.3V)
-input("Press Enter to continue...")
+
+# There are a three ways to set the DAC output, you can use any of these:
+dac.value = 65535  # Use the value property with a 16-bit number just like
+# the AnalogOut class.  Note the MCP4725 is only a 12-bit
+# DAC so quantization errors will occur.  The range of
+# values is 0 (minimum/ground) to 65535 (maximum/Vout).
+
+dac.raw_value = 4095  # Use the raw_value property to directly read and write
+# the 12-bit DAC value.  The range of values is
+# 0 (minimum/ground) to 4095 (maximum/Vout).
+
+dac.normalized_value = 1.0  # Use the normalized_value property to set the
+# output with a floating point value in the range
+# 0 to 1.0 where 0 is minimum/ground and 1.0 is
+# maximum/Vout.
+
+# Main loop will go up and down through the range of DAC values forever.
+while True:
+    # Go up the 12-bit raw range.
+    print("Going up 0-3.3V...")
+    for i in range(4095):
+        dac.raw_value = i
+    # Go back down the 12-bit raw range.
+    print("Going down 3.3-0V...")
+    for i in range(4095, -1, -1):
+        dac.raw_value = i
