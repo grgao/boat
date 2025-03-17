711cfffe5a8404c2713bf32b54b55f44e9df1404
Grace Gao <gegao@wisc.edu>
2025-03-17 17:16:32 -0500

    help

--- ADC_sine_test.py
+++ ADC_sine_test.py
@@ -9,4 +9,10 @@ i2c = busio.I2C(board.SCL, board.SDA)
 dac = adafruit_mcp4725.MCP4725(i2c, address=0x60)
 
 
-dac.raw_value = 2232  # Should be ~1.80V
+# Test output voltages
+dac.raw_value = 0       # Should output ~0V
+input("Press Enter to continue...")
+dac.raw_value = 2048    # Should output ~1.65V (if VREF = 3.3V)
+input("Press Enter to continue...")
+dac.raw_value = 4095    # Should output ~3.3V (if VREF = 3.3V)
+input("Press Enter to continue...")
